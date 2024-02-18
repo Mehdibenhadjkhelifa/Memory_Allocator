22a37434319a18c82170c8e10030090858c18917
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2024-02-18 15:09:05 +0100

    Implemented Malloc and some helper functions
    TODO: Implement a free to deallocate blocks of memory

--- src/main.c
+++ src/main.c
@@ -4,26 +4,68 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <stdio.h>
-void* heap_alloc(size_t size){
-    void* p = sbrk(0);
-    void* request = sbrk((ssize_t)size);
-    if(request == (void*) -1){
-        fprintf(stderr,"Sbrk failed to allocate %zu bytes\n",size);
-       return NULL;
+
+void* global_base = NULL;
+#define META_SIZE sizeof(block_meta)
+typedef struct block_meta{
+    size_t size;
+    struct block_meta* next;
+    int free;
+    int magic;
+}block_meta;
+
+block_meta* find_free_block(block_meta** last,size_t size){
+    block_meta* current = global_base;
+    while (current && !(current->free && current->size >= size)){
+        *last = current;
+        current = current->next;
     }
-    else{
-        assert(p == request);//not thread safe 
-        fprintf(stdout,"successfully increased space by %zu bytes\n",size);
-        return p;                     
-    } 
+    return current;
 }
+block_meta* request_space(block_meta* last,size_t size){
+    block_meta* block;
+    block = sbrk(0);
+    void* request = sbrk((intptr_t)(size + META_SIZE));
+    assert((void*)block == request);
+    if(request == (void*) -1){
+        return NULL;
+    }
+    if(last)
+        last->next = block;
+    block->size = size;
+    block->next = NULL;
+    block->free = 0;
+    block->magic = 0x12345678;
+    return block;
+}
+
+void* malloc(size_t size){
+    block_meta* block;
+    //TODO: Align size
+    if(size <= 0)
+        return NULL;
+    if(!global_base){//First call
+        block = request_space(NULL,size);
+        if(block == NULL)
+            return NULL;
+        global_base = block;
+    }else{
+        block_meta* last = global_base;
+        block = find_free_block(&last,size);
+        if(block == NULL){//Failed to find free block
+            block = request_space(last,size);
+            if(!block)
+                return NULL;//Requesting memory failed
+        }else{
+            //TODO: consider splitting block here
+            block->free = 0;
+            block->magic = 0x77777777;
+        }
+    }
+    return (block+1);
+}
+
 int main(void){
-    size_t arr_size = 5;
-    int* test = (int*)heap_alloc( arr_size * sizeof(int));
-    for(size_t i = 0;i<arr_size; i++){
-        test[i] = (int)i;
-    }
-    for(size_t i = 0;i< arr_size; i++){
-        printf("test of %zu is : %d\n",i,test[i]);
-    }
+    printf("size of meta block is %lu\n",META_SIZE);
+    printf("size of size_t : %zu\n size of void* : %lu\n size of int : %lu\n",sizeof(size_t),sizeof(void*),sizeof(int));
 }
