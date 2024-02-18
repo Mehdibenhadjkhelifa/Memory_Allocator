72e78cd334facf9e04327e8489f2c4f1faa835d2
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2024-02-18 17:11:51 +0100

    Implemented simple realloc and calloc
    TODO: Debug current code and use it as a shared libary in another program
    
    TODO: check if malloc returns a pointer which is suitably aligned for any built-in type
    
    TODO: Implement a function to split up blocks so that they use minimum amount of space necessary
    
    TODO: Implement a mechanism to merge adjacent free blocks

--- src/memory.c
+++ src/memory.c
@@ -1,6 +1,6 @@
 #include <stddef.h>
 #include <assert.h>
-
+#include <string.h>
 #include "core.h"
 #include "memory.h"
 extern void* global_base;
@@ -29,6 +29,27 @@ void* malloc(size_t size){
         }
     }
     return (block+1);
+}
+void* realloc(void* ptr,size_t size){
+    if(!ptr)
+        return malloc(size);//NULL ptr. realloc should act like malloc
+    block_meta* block_ptr = get_block_ptr(ptr);
+    if(block_ptr->size >= size)
+        return ptr; //We have enough space . Could free some once we implement split
+    // Need to really realloc. Malloc new space and free old space.
+    // Then copy old data to new space.
+    void* new_ptr = malloc(size);
+    if(!new_ptr)
+        return NULL; // set errno on failure
+    memcpy(new_ptr,ptr,block_ptr->size);
+    free(ptr);
+    return new_ptr;
+}
+void* calloc(size_t nelem,size_t elsize){
+    size_t size = nelem * elsize; //TODO: check for overflow
+    void* ptr = malloc(size);
+    memset(ptr,0,size);
+    return ptr;
 }
 void free(void* ptr){
     if(!ptr)
--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,8 @@
 #ifndef _MEMORY_H
 #define _MEMORY_H
 void* malloc(size_t size);
+void* realloc(void* ptr,size_t size);
+void* calloc(size_t nelem,size_t elsize);
 void free(void* ptr);
 #endif
 
